4c6add22d7153c7b98285f9b95f8bdade69eff38
Brandon Cotten <brandon__cotten@hotmail.com>
2025-04-09 11:33:39 -0700

    mobile nav and some .jsx to .tsx fixes made

--- src/components/my-components/Navigation.tsx
+++ src/components/my-components/Navigation.tsx
@@ -1,9 +1,27 @@
-
+import logo from '../../assets/logo.svg';
 
 const Navigation = () => {
   return (
-    <h1>Navigation</h1>
-  )
-}
+    <nav className="bg-background p-4">
+      <div className="flex flex-row px-4">
+        <a href="#" className="flex items-center gap-2">
+          <img src={logo} alt="Logo" className='max-w-32' />
+        </a>
+
+        <div className="text-foreground px-8 font-base text-step-0 flex flex-col gap-2">
+          <a href="#projects" className="hover:text-accent">
+            Projects
+          </a>
+          <a href="#about" className="hover:text-accent">
+            About
+          </a>
+          <a href="#contact" className="hover:text-accent">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navigation;
