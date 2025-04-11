4ebd58395cb60ecc083a20d2a64ac7e99ae02c35
Brandon Cotten <brandon__cotten@hotmail.com>
2025-04-11 16:26:59 -0700

    projects carousel started changed project data in content and added a projects.json file

--- src/components/my-components/Hero.tsx
+++ src/components/my-components/Hero.tsx
@@ -19,38 +19,31 @@ const imageMap: ImageMap = {
 
 const Hero = () => {
   return (
-    <section className="text-foreground mx-4 pt-8">
-      <h1 className="text-step-3 font-title">{content.hero.name}</h1>
-      <h2 className="text-step-2 font-title max-w-44">{content.hero.title}</h2>
-      <div className="grid grid-cols-2 grid-rows-2 gap-4 pt-4 ">
-        {content.hero.cards.map((card, index) => (
-          <div
-            key={index}
-            className={clsx(
-              "relative h-full w-full",
-              {
-                "row-start-2 col-start-1": index === 2,
-                "row-start-1 col-start-2": index === 0,
-                "row-start-2 col-start-2": index === 1,
-              }
-            )}
-          >
-            <AspectRatio ratio={1 / 1} className="h-full">
-              <img
-                src={imageMap[card.img]}
-                alt={card.title}
-                className="absolute inset-0 h-full w-full rounded-md object-cover"
-              />
-            </AspectRatio>
-            <div className='absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-              <Button size="sm" variant="secondary" className='p-4'>
-                <a href={card.link}>{card.title}</a>
-              </Button>
-            </div>
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 pt-4">
+      {content.hero.cards.map((card, index) => (
+        <div
+          key={index}
+          className={clsx('relative h-full w-full', {
+            'col-start-1 row-start-2': index === 2,
+            'col-start-2 row-start-1': index === 0,
+            'col-start-2 row-start-2': index === 1,
+          })}
+        >
+          <AspectRatio ratio={1 / 1} className="h-full">
+            <img
+              src={imageMap[card.img]}
+              alt={card.title}
+              className="absolute inset-0 h-full w-full rounded-md object-cover"
+            />
+          </AspectRatio>
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Button size="sm" variant="secondary" className="p-4">
+              <a href={card.link}>{card.title}</a>
+            </Button>
           </div>
-        ))}
-      </div>
-    </section>
+        </div>
+      ))}
+    </div>
   );
 };
 
--- src/components/my-components/Projects.tsx
+++ src/components/my-components/Projects.tsx
@@ -1,9 +1,39 @@
+import content from '../../data/content.json';
 
+import { Button } from '../ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 const Projects = () => {
   return (
-    <h1>Projects</h1>
-  )
-}
+    <Carousel
+      opts={{ loop: true, dragFree: false, align: 'center' }}
+      className="pt-8"
+    >
+      <CarouselContent>
+        {content.projects.cards.map((card, index) => (
+          <CarouselItem
+            className="bg-primary flex flex-col gap-4 rounded-md"
+            key={index}
+          >
+            <img src={card.image} alt={card.title} className="rounded-md" />
+            <h3>{card.title}</h3>
+            <p>{card.description}</p>
+            <Button size="sm" variant="secondary" className="p-4">
+              <a href={card.link}>View Project</a>
+            </Button>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
 
 export default Projects;
