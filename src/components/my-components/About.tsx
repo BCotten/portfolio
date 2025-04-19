7b3121ec5c300620776983152d7425265adb7476
Brandon Cotten <brandon__cotten@hotmail.com>
2025-04-19 13:30:58 -0700

    added icons for skills and tools added types folder and types conent.json

--- src/components/my-components/About.tsx
+++ src/components/my-components/About.tsx
@@ -1,9 +1,59 @@
+import content from '@/data/content.json';
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 const About = () => {
   return (
-    <h1>About</h1>
-  )
-}
+    <>
+      <Card className="mx-auto mt-4 max-w-xl">
+        <CardHeader>
+          <CardTitle className="font-title text-step-1">
+            {content.about.cards[0].title}
+          </CardTitle>
+          <CardDescription>
+            {content.about.cards[0].year ?? 'Year.. seems to be missing'}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="text-step--1">
+          <p>
+            {content.about.cards[0].content ?? 'Content... seems to be missing'}
+          </p>
+        </CardContent>
+      </Card>
+      <Card className="mx-auto mt-4 max-w-xl">
+        <CardHeader>
+          <CardTitle className="font-title text-step-1">
+            {content.about.cards[1].title}
+          </CardTitle>
+        </CardHeader>
+        <CardContent className="text-step--1">
+          <p>{content.about.cards[1].skills?.[0]?.name ?? 'SKill'}</p>
+          <p>{content.about.cards[1].skills?.[0]?.icon ?? 'Icon'}</p>
+        </CardContent>
+      </Card>
+      <Card className="mx-auto mt-4 max-w-xl">
+        <CardHeader>
+          <CardTitle className="font-title text-step-1">
+            {content.about.cards[2].title}
+          </CardTitle>
+          <CardDescription>
+            {content.about.cards[2].year ?? 'Year.. seems to be missing'}
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="text-step--1">
+          <p>
+            {content.about.cards[2].content ?? 'Content... seems to be missing'}
+          </p>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
 
 export default About;
